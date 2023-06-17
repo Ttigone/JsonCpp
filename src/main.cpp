2f78a539abce31b1b875c3a4d4ba707af073fb67
Ttigone <2523849921@qq.com>
2023-06-17 23:59:39 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "json.h"
 
-signed main()
+using namespace css::json;
+
+int main()
 {
-    Json a;
-    a.f();
+    Json v1;
+    Json v2 = true;
+    Json v3 = 123;
+    Json v4 = 1.23;
+    Json v5 = "Hello world";
+
 }
